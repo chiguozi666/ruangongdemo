79624ebba598c9dd88ac5ea24a82e4be55c880ab
chiguozizi <1060020877@qq.com>
2020-11-04 15:19:27 +0800

    初始创建

---
+++ src/main/java/com/mingyuan/bangbang/BangbangApplication.java
@@ -0,0 +1,13 @@
+package com.mingyuan.bangbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangbangApplication.class, args);
+    }
+
+}
